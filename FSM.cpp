5e32cca5b373acff44e2ac0934259886a12ea3d4
LucasHasting <110124874+LucasHasting@users.noreply.github.com>
2024-07-30 21:08:16 -0500

    added comments

--- FSM.cpp
+++ FSM.cpp
@@ -1,5 +1,10 @@
 #include "FSM.h"
 
+/*
+Function Name: 
+Function Description: The function uses an input stream of 1s and 0s 
+                      to find the solution to the FSM
+*/
 void find_solution(Node* start, string input_stream){
     Node* current = start;
     int length = strlen(input_stream.c_str());
@@ -21,11 +26,22 @@ void find_solution(Node* start, string input_stream){
 
 }
 
+/*
+Function Name: connect_graph
+Function Description: Connect a Node to a corresponding node based on its state.
+                      Node one is the node the graph connects to if the input is 1.
+                      Node zero is the node the graph connects to if the input is 0.
+                      Nodes can connect to themselves.
+*/
 void connect_graph(Node* add, Node* zero, Node* one){
     add->one = one;
     add->zero = zero;
 }
 
+/*
+Function Name: load_from_file
+Function Description: loads FMS information from a state file
+*/
 Node** load_from_file(string data_file, int &size){
     ifstream in_file(data_file);
     in_file >> size;
@@ -48,23 +64,28 @@ Node** load_from_file(string data_file, int &size){
 
 }
 
+/*
+Structure of file:
+amount_of_states state_1_output ... state_n_output
+state_0_connection_0 state_0_connection_1
+...
+*/
+
+/*
+state connections are indexs to the array, first state is 0, all states are represented with numbers.
+
+example file:
+3 0 1 1
+0 1
+0 0
+1 1
+*/
+
+/*
+Function Name: make_file
+Function Description: makes a state file which will be read by the program
+*/
 void make_file(string data_file){
-    /*
-    Structure of file:
-    amount_of_states state_1_output ... state_n_output
-    state_0_connection_0 state_0_connection_1
-    ...
-    */
-
-    /*
-    state connections are indexs to the array, first state is 0, all states are represented with numbers
-    example file:
-    3 0 1 1
-    0 1
-    0 0
-    1 1
-    */
-
     int size;
     ofstream out_file;
     out_file.open(data_file);
@@ -95,6 +116,10 @@ void make_file(string data_file){
     out_file.close();
 }
 
+/*
+Function Name: display_menu
+Function Description: The main menu the user sees
+*/
 void display_menu(){
     cout << "make state machine file \t Enter 1 \n"
     << "load state machine from file \t Enter 2 \n"
@@ -102,6 +127,10 @@ void display_menu(){
     << "Exit \t\t\t\t Enter 0 \nEnter: ";
 }
 
+/*
+Function Name: delete_nodes
+Function Description: delete a node from the FSM
+*/
 void delete_nodes(Node** data, int size){
     if (data == nullptr){
         return;
--- Node.cpp
+++ Node.cpp
@@ -1,9 +1,11 @@
 #include "node.h"
 
+//default constructor
 Node::Node(){
     data = 0;
 }
 
+//constructor with input
 Node::Node(bool output){
     data = output;
 }
